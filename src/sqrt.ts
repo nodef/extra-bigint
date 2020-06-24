ff96f7f6f5ae6863c5fdc43f21896072cf64a9df
Subhajit Sahu <wolfram77@gmail.com>
2020-06-25 00:45:21 +0530

    :coconut: chutney ;sqrt

--- src/sqrt.ts
+++ src/sqrt.ts
@@ -1,15 +1,18 @@
-/**
- * Gives square root of value.
- * @param x a bigint
- */
-function sqrt(x: bigint): bigint {
-  if(x<0n) return null;
-  if(x===0n) return 0n;
+function sqrtPos(x: bigint): bigint {
   var a = x, b = x+1n;
   while(a<b) {
     b = a;
     a = (b + x/b)/2n;
   }
   return b;
+}
+
+/**
+ * Gives square root of value.
+ * @param x a bigint
+ */
+function sqrt(x: bigint): bigint {
+  if(x===0n) return 0n;
+  else return x>0n? sqrtPos(x) : null;
 }
 export default sqrt;
